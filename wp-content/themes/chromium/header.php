89e5f6beb8429db29d2bd17b17d39b7171f4f6e3
Artur Meshchenkov <artur133715@gmail.com>
2021-04-14 10:58:30 +0600

    Проверка коммита

--- wp-content/themes/chromium/header.php
+++ wp-content/themes/chromium/header.php
@@ -16,11 +16,29 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<?php endif; ?>
 		<?php wp_head(); ?>
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(65800849, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
+            });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/65800849" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
 	</head>
 
 	<body <?php body_class(); ?>>
 
+
 		<div id="page" class="site"><!-- Site's Wrapper -->
+            <div class="bg__filter"></div>
 
 			<header class="site-header"><!-- Site's Header -->
                 <?php do_action( 'chromium_before_header' ); ?>
